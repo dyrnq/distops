cef6af61a19fd2d0233ebdfcb3d9cc53792ca07e
dyrnq <dyrnq@qq.com>
2026-05-26 10:13:46 +0800

    fix: rewrite BlobMapper/ManifestBlobMapper as plain beans with @Inject DbContext (not BaseMapperWrap)

--- src/main/java/com/dyrnq/distops/dso/BlobMapper.java
+++ src/main/java/com/dyrnq/distops/dso/BlobMapper.java
@@ -2,15 +2,38 @@ package com.dyrnq.distops.dso;
 
 import com.dyrnq.distops.model.Blob;
 import org.noear.solon.annotation.Component;
-import org.noear.wood.mapper.BaseMapperWrap;
+import org.noear.solon.annotation.Inject;
+import org.noear.wood.DbContext;
 
 @Component
-public class BlobMapper extends BaseMapperWrap<Blob> {
-    public BlobMapper() {
-        super(null, Blob.class, Blob.TABLE_NAME);
-    }
+public class BlobMapper {
+    @Inject
+    DbContext dbContext;
 
     public Blob findByInstIdAndDigest(Long instId, String digest) {
-        return selectItem(m -> m.whereEq(Blob.INST_ID, instId).andEq(Blob.DIGEST, digest));
+        try {
+            return dbContext.table(Blob.TABLE_NAME)
+                    .whereEq(Blob.INST_ID, instId)
+                    .andEq(Blob.DIGEST, digest)
+                    .selectItem("*", Blob.class);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void insert(Blob blob, boolean replace) {
+        try {
+            dbContext.table(Blob.TABLE_NAME).setEntity(blob).insert();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void updateById(Blob blob, boolean replace) {
+        try {
+            dbContext.table(Blob.TABLE_NAME).setEntity(blob).whereEq(Blob.ID, blob.getId()).update();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/com/dyrnq/distops/dso/ManifestBlobMapper.java
+++ src/main/java/com/dyrnq/distops/dso/ManifestBlobMapper.java
@@ -2,11 +2,19 @@ package com.dyrnq.distops.dso;
 
 import com.dyrnq.distops.model.ManifestBlob;
 import org.noear.solon.annotation.Component;
-import org.noear.wood.mapper.BaseMapperWrap;
+import org.noear.solon.annotation.Inject;
+import org.noear.wood.DbContext;
 
 @Component
-public class ManifestBlobMapper extends BaseMapperWrap<ManifestBlob> {
-    public ManifestBlobMapper() {
-        super(null, ManifestBlob.class, ManifestBlob.TABLE_NAME);
+public class ManifestBlobMapper {
+    @Inject
+    DbContext dbContext;
+
+    public void insert(ManifestBlob mb, boolean replace) {
+        try {
+            dbContext.table(ManifestBlob.TABLE_NAME).setEntity(mb).insert();
+        } catch (java.sql.SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
